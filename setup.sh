feb9acd2dc6a094aa78d470b67fd0a5cc0654529
Maxim Kovgan <maxk@devopsent.biz>
2016-06-23 18:42:19 +0300

    updated setup + requirements
    Signed-off-by: Maxim Kovgan <maxk@devopsent.biz>

--- setup.sh
+++ setup.sh
@@ -1,21 +1,44 @@
 #!/usr/bin/env bash
 
 set -o xtrace
+set -o errexit
 
-items=(
-    "branch"
-    "diff"
-    "grep"
-    "interactive"
-    "pager"
-    "status"
+packages=(
+    "build-essential"
+    "libffi-dev"
+    "libssl-dev"
+    "virtualenvwrapper"
+    "python-dev"
+    "vim"
+    "git-core"
+)
+# packages
+sudo apt-get install "${packages[@]}"
+
+git_config_items=(
+    "color.branch"
+    "color.diff"
+    "color.grep"
+    "color.interactive"
+    "color.pager"
+    "color.status"
 )
 
-for item in "${items[@]}"; do
-  git config --global color.${item} true
+for item in "${git_config_items[@]}"; do
+  git config --global ${item} true
 done
+git config --global core.editor /usr/bin/vim
+
+# bashit
+! [[ -d "${HOME}/.bash_it" ]] && git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+# pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+! [[ -r "${HOME}/.vim/autoload/pathogen.vim" ]] \
+&& curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+source "${HOME}/.bashrc"
+lsvirtualenv -b | grep -E "ansible-prod" &> /dev/null \
+|| mkvirtualenv ansible-prod
 
-sudo apt-get install build-essential libffi-dev libssl-dev
+pip install -r ./requirements.txt
